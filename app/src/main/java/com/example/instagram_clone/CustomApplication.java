2fc0a967cb1ff71ce5abbca86dcc090c3512b1c5
Leonardo Rick <leonardo@lanytecnologia.com.br>
2020-08-03 22:44:30 -0300

    Update CustomApplication.java

--- app/src/main/java/com/example/instagram_clone/CustomApplication.java
+++ app/src/main/java/com/example/instagram_clone/CustomApplication.java
@@ -2,7 +2,6 @@ package com.example.instagram_clone;
 
 import android.app.Application;
 import android.content.Context;
-import android.widget.Toast;
 
 public class CustomApplication extends Application {
     private static Context context;
